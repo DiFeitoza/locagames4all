37ffb47e01f13c217af193a1a701f4850cfaa1a4
difeitoza <diegofeitozabr@gmail.com>
2022-06-17 16:45:47 -0300

    first commit

---
+++ src/main/java/ufc/pds/locagames4all/Locagames4allApplication.java
@@ -0,0 +1,13 @@
+package ufc.pds.locagames4all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Locagames4allApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Locagames4allApplication.class, args);
+    }
+
+}
