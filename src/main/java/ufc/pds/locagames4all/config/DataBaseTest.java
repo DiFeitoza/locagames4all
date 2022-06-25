e96147ae3151644f64c1c84ff0614c87c23d150b
ec-ayrton <ec.ayrton@gmail.com>
2022-06-25 16:00:56 -0300

    Criacao das services dos jogos e algumas consultas

--- src/main/java/ufc/pds/locagames4all/config/DataBaseTest.java
+++ src/main/java/ufc/pds/locagames4all/config/DataBaseTest.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import ufc.pds.locagames4all.enums.StatusJogo;
+import ufc.pds.locagames4all.enums.TipoJogo;
 import ufc.pds.locagames4all.model.Cliente;
+import ufc.pds.locagames4all.model.Jogo;
 import ufc.pds.locagames4all.repositories.ClienteRepositoryJPA;
+import ufc.pds.locagames4all.repositories.JogoRepositoryJPA;
 
 import java.util.Arrays;
 
@@ -16,6 +20,9 @@ public class DataBaseTest implements CommandLineRunner {
     @Autowired
     private ClienteRepositoryJPA clienteRepository;
 
+    @Autowired
+    private JogoRepositoryJPA jogoRepositoryJPA;
+
     @Override
     public void run(String... args) throws Exception {
         Cliente c1 = new Cliente("Ana", "11122233344", "rua JK, 72","8899911223344","ana@bol.com");
@@ -25,5 +32,13 @@ public class DataBaseTest implements CommandLineRunner {
 
 
         clienteRepository.saveAll(Arrays.asList(c1,c2,c3,c4));
+
+        Jogo j1 = new Jogo("LoTR", TipoJogo.RPG,"Senhor dos aneis",1,8,15.0);
+        Jogo j2 = new Jogo("Xadrez", TipoJogo.TABULEIRO,"Xadrez",2,2,10.0);
+        Jogo j3 = new Jogo("Dama", TipoJogo.TABULEIRO,"Dama",2,2,10.0);
+        Jogo j4 = new Jogo("Poker", TipoJogo.CARTAS,"Baralho para Poker",2,9,12.0);
+
+        jogoRepositoryJPA.saveAll(Arrays.asList(j1,j2,j3,j4));
+
     }
 }
