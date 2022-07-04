14040606886a1bb33030ab65161e734d13cca7b0
difeitoza <diegofeitozabr@gmail.com>
2022-07-04 14:43:12 -0300

    Implementa o RootEntryPoint da API.

---
+++ src/main/java/ufc/pds/locagames4all/controllers/RootEntryPointController.java
@@ -0,0 +1,28 @@
+package ufc.pds.locagames4all.controllers;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ufc.pds.locagames4all.model.RootEntryPoint;
+
+@RestController
+public class RootEntryPointController {
+    @GetMapping
+    public RootEntryPoint root() {
+        var model = new RootEntryPoint();
+        String root = linkTo(RootEntryPointController.class).toUri().toString();
+
+        model.add(Link.of(root + "/swagger-ui/index.html").withRel("documentacao_swagger"));
+        model.add(linkTo(methodOn(ClienteController.class)
+                .buscarClientes()).withRel("clientes"));
+        model.add(linkTo(methodOn(JogoController.class)
+                .buscarJogos()).withRel("jogos"));
+        model.add(linkTo(methodOn(LocacaoController.class)
+                .buscarLocacoes()).withRel("locacoes"));
+        return model;
+    }
+}
