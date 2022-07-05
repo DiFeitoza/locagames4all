f1d555ead6aec17788466f734cb3ac7a0d482c2c
difeitoza <diegofeitozabr@gmail.com>
2022-07-05 06:23:09 -0300

    Melhora a documentação, remove links do schema no swagger.

--- src/main/java/ufc/pds/locagames4all/config/SwaggerConfig.java
+++ src/main/java/ufc/pds/locagames4all/config/SwaggerConfig.java
@@ -4,9 +4,10 @@ package ufc.pds.locagames4all.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.hateoas.Links;
 
 
 @Configuration
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI LocaGames4all() {
+        SpringDocUtils.getConfig().addResponseTypeToIgnore(Links.class);
         return new OpenAPI()
                 .info(new Info().title("LocaGames4all API")
                         .description("Sua API de locação de jogos de mesa!")
